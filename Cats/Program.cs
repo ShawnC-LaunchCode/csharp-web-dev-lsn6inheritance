c4998ca5e1465aedf656ff0685b745c94c35ead9
Shawn C <scooter4356@gmail.com>
2020-07-01 18:41:49 -0500

    after messing around with it

--- Cats/Program.cs
+++ Cats/Program.cs
@@ -7,6 +7,16 @@ namespace Cats
         static void Main(string[] args)
         {
             // Try out your skills with inheritance here!
+
+            Cat a = new Cat(42);
+
+            Console.WriteLine(a.Hungry);
+
+
+            HouseCat b = new HouseCat("bert", 19);
+
+            Console.WriteLine(b.Noise());
+
         }
     }
 }
